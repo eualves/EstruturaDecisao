1c68aa90541c562a281c0d12010fa7e2ea51c197
Leonardo <leoalvesbico@gmail.com>
2021-09-23 11:36:17 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDecisão/Estrutura.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace EstruturaDecisão
+{
+    partial class TelaEstrutura
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl01 = new System.Windows.Forms.Label();
+            this.lbl03 = new System.Windows.Forms.Label();
+            this.lbl02 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnDescobrir = new System.Windows.Forms.Button();
+            this.Num01 = new System.Windows.Forms.NumericUpDown();
+            this.Num02 = new System.Windows.Forms.NumericUpDown();
+            this.Num03 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Num01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Num02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Num03)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Qual é o maior Valor?";
+            // 
+            // lbl01
+            // 
+            this.lbl01.AutoSize = true;
+            this.lbl01.BackColor = System.Drawing.Color.Transparent;
+            this.lbl01.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl01.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl01.Location = new System.Drawing.Point(69, 84);
+            this.lbl01.Name = "lbl01";
+            this.lbl01.Size = new System.Drawing.Size(81, 22);
+            this.lbl01.TabIndex = 1;
+            this.lbl01.Text = "Valor 1:";
+            // 
+            // lbl03
+            // 
+            this.lbl03.AutoSize = true;
+            this.lbl03.BackColor = System.Drawing.Color.Transparent;
+            this.lbl03.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl03.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl03.Location = new System.Drawing.Point(69, 184);
+            this.lbl03.Name = "lbl03";
+            this.lbl03.Size = new System.Drawing.Size(81, 22);
+            this.lbl03.TabIndex = 2;
+            this.lbl03.Text = "Valor 3:";
+            // 
+            // lbl02
+            // 
+            this.lbl02.AutoSize = true;
+            this.lbl02.BackColor = System.Drawing.Color.Transparent;
+            this.lbl02.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl02.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl02.Location = new System.Drawing.Point(69, 136);
+            this.lbl02.Name = "lbl02";
+            this.lbl02.Size = new System.Drawing.Size(81, 22);
+            this.lbl02.TabIndex = 3;
+            this.lbl02.Text = "Valor 2:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lblResultado.Location = new System.Drawing.Point(34, 296);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(261, 58);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDescobrir
+            // 
+            this.btnDescobrir.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDescobrir.Location = new System.Drawing.Point(83, 228);
+            this.btnDescobrir.Name = "btnDescobrir";
+            this.btnDescobrir.Size = new System.Drawing.Size(166, 40);
+            this.btnDescobrir.TabIndex = 5;
+            this.btnDescobrir.Text = "Descobrir";
+            this.btnDescobrir.UseVisualStyleBackColor = true;
+            this.btnDescobrir.Click += new System.EventHandler(this.btnDescobrir_Click);
+            // 
+            // Num01
+            // 
+            this.Num01.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num01.Location = new System.Drawing.Point(165, 77);
+            this.Num01.Name = "Num01";
+            this.Num01.Size = new System.Drawing.Size(84, 29);
+            this.Num01.TabIndex = 6;
+            this.Num01.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Num02
+            // 
+            this.Num02.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num02.Location = new System.Drawing.Point(165, 129);
+            this.Num02.Name = "Num02";
+            this.Num02.Size = new System.Drawing.Size(84, 29);
+            this.Num02.TabIndex = 7;
+            this.Num02.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Num03
+            // 
+            this.Num03.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num03.Location = new System.Drawing.Point(165, 177);
+            this.Num03.Name = "Num03";
+            this.Num03.Size = new System.Drawing.Size(84, 29);
+            this.Num03.TabIndex = 8;
+            this.Num03.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TelaEstrutura
+            // 
+            this.AcceptButton = this.btnDescobrir;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkViolet;
+            this.ClientSize = new System.Drawing.Size(329, 378);
+            this.Controls.Add(this.Num03);
+            this.Controls.Add(this.Num02);
+            this.Controls.Add(this.Num01);
+            this.Controls.Add(this.btnDescobrir);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lbl02);
+            this.Controls.Add(this.lbl03);
+            this.Controls.Add(this.lbl01);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "TelaEstrutura";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estrutura de decisão";
+            this.Load += new System.EventHandler(this.TelaEstrutura_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Num01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Num02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Num03)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl01;
+        private System.Windows.Forms.Label lbl03;
+        private System.Windows.Forms.Label lbl02;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnDescobrir;
+
+        private System.Windows.Forms.NumericUpDown Num01;
+        private System.Windows.Forms.NumericUpDown Num02;
+        private System.Windows.Forms.NumericUpDown Num03;
+    }
+}
+
---
+++ EstruturaDecisão/Estrutura.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstruturaDecisão
+{
+    public partial class TelaEstrutura : Form
+    {
+        public TelaEstrutura()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDescobrir_Click(object sender, EventArgs e)
+        {
+            int valor01 = Convert.ToInt32(Num01.Value);
+            int valor02 = Convert.ToInt32(Num02.Value);
+            int valor03 = Convert.ToInt32(Num03.Value);
+
+            if (valor01 == valor02 && valor02 == valor03)
+            {
+                lblResultado.Text = "Os valores são iguais!";
+                return;
+            }
+            if (valor01 > valor02 && valor01 > valor03)
+            {
+                lblResultado.Text = "O Valor 1 é o maior!";
+            }else if (valor02 > valor01 && valor02 > valor03){
+                lblResultado.Text = "O Valor 2 é o maior!";
+                return;
+            } else if (valor03 > valor01 && valor03 > valor02)
+            {
+                lblResultado.Text = "O Valor 3 é o maior!";
+                return;
+            }
+
+            if (valor01 == valor02 && valor01 > valor03 && valor02 > valor03)
+            {
+                lblResultado.Text = "Os valores 1 e 2 são maiores!";
+                return;
+            }else if (valor01 == valor03 && valor01 > valor02 && valor03 > valor02)
+            {
+                lblResultado.Text = "Os valores 1 e 3 são maiores!";
+                return;
+            }
+            else if (valor02 == valor03 && valor02 > valor01 && valor03 > valor01)
+            {
+                lblResultado.Text = "Os valores 2 e 3 são maiores!";
+                return;
+            }
+        }
+
+
+
+        private void TelaEstrutura_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
